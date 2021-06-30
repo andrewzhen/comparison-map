970bc4a613f49c76e4c3f20f77ad908520416eae
andrewzhen <ajzhen@ucsd.edu>
2021-06-29 18:18:50 -0700

    popup complete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import { useEffect } from 'react';
 import './index.scss';
 import Logo from './assets/amply-logo.svg';
+import LightDuty from './assets/light-duty-fleet.svg';
+import MediumDuty from './assets/medium-duty-fleet.svg';
+import Bus from './assets/bus-fleet.svg';
+import HeavyDuty from './assets/heavy-duty-fleet.svg';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
@@ -8,8 +12,8 @@ import cities from "./cities.json";
 
 export default function App() {
   const TOKEN = "pk.eyJ1IjoiYW1wbHkiLCJhIjoiY2tuMm93aGdvMTlibzJvbWx4dmo2a2lhMiJ9.fHpj1_WDJTJSG68nASen1w";
-  const CENTER = { lat: 37.396340, lng: -122.058760 };
-  const ZOOM = 8;
+  const CENTER = { lat: 37.0902, lng: -95.7129 };
+  const ZOOM = 5;
   
   // custom Marker Icon
   L.Marker.prototype.options.icon = L.divIcon({
@@ -22,8 +26,6 @@ export default function App() {
       let icons = [...document.getElementsByClassName("icon-span")];
       icons.forEach((icon, idx) => icon.textContent = cities[idx].name);
     }, 10);
-
-    document.getElementsByClassName("leaflet-popup-close-button")[0].textContent = "";
   }, []);
 
   return (
@@ -48,23 +50,130 @@ export default function App() {
             return (
               <Marker key={idx} position={city.coords}>
                 <Popup autoPan={false}>
-                  <div>{city.name}</div>
-                  <div>{city.subhead}</div>
+                  <header>
+                    <h1 className="cityName">{city.name}</h1>
+                    <h2 className="citySubhead">{city.subhead}</h2>
+                  </header>
+
+                  <div className="fleet-chart">
+                    <div className="fleet-chart__row title">
+                      <h3> </h3>
+                      <h3>Fleet Type</h3>
+                      <h3>Managed Electric</h3>
+                      <h3>Unmanaged Electric</h3>
+                      <h3>Gas or Diesel</h3>
+                    </div>
+
+                    <hr />
+                    
+                    <div className="fleet-chart__row">
+                      <img src={LightDuty} alt="Light Duty" />
+                      <p className="fleet-chart__row__type">Light-Duty</p>
+                      {
+                        city.vehicles.light.map((price, idx) => {
+                          return (
+                            <p 
+                              key={idx} 
+                              className={
+                                "fleet-chart__row__cost " + 
+                                (idx === 0 && "managed_electric")
+                              }
+                            >
+                              {price}
+                            </p>
+                          )
+                        })
+                      }
+                    </div>
+                    
+                    <hr />
+
+                    <div className="fleet-chart__row">
+                      <img src={MediumDuty} alt="Medium Duty" />
+                      <p className="fleet-chart__row__type">Medium-Duty</p>
+                      {
+                        city.vehicles.medium.map((price, idx) => {
+                          return (
+                            <p 
+                              key={idx} 
+                              className={
+                                "fleet-chart__row__cost " + 
+                                (idx === 0 && "managed_electric")
+                              }
+                            >
+                              {price}
+                            </p>
+                          )
+                        })
+                      }
+                    </div>
+                    
+                    <hr />
+
+                    <div className="fleet-chart__row">
+                      <img src={Bus} alt="Bus" />
+                      <p className="fleet-chart__row__type">City Bus</p>
+                      {
+                        city.vehicles.bus.map((price, idx) => {
+                          return (
+                            <p 
+                              key={idx} 
+                              className={
+                                "fleet-chart__row__cost " + 
+                                (idx === 0 && "managed_electric")
+                              }
+                            >
+                              {price}
+                            </p>
+                          )
+                        })
+                      }
+                    </div>
+
+                    <hr />
+
+                    <div className="fleet-chart__row">
+                      <img src={HeavyDuty} alt="Heavy Duty" />
+                      <p className="fleet-chart__row__type">Heavy-Duty</p>
+                      {
+                        city.vehicles.heavy.map((price, idx) => {
+                          return (
+                            <p 
+                              key={idx} 
+                              className={
+                                "fleet-chart__row__cost " + 
+                                (idx === 0 && "managed_electric")
+                              }
+                            >
+                              {price}
+                            </p>
+                          )
+                        })
+                      }
+                    </div>
+
+                    <hr />
+                  </div>
+
+                  <footer>
+                    *Utilities with proposed special EV charging rates Gasoline (light- and medium-duty) and diesel (heavy-duty and city buses) prices eﬀective as of 02/23/2020. Source: AAA (https://gas.prices.aaa.com) For all the Metros, managed charging unlocks larger savings. AMPLY handles all aspects of charging operations on behalf of ﬂeet owners, and AMPLY’s managed charging systems are optimized for the lowest electricity costs through navigating demand charges and diﬀerent tariﬀ rates.
+                  </footer>
                 </Popup>
               </Marker>
             )
           })
         }
         <TileLayer 
-          url={"https://api.mapbox.com/styles/v1/amply/ckp796vza066p17qw072dnhmr/tiles/256/{z}/{x}/{y}@2x?access_token=" + TOKEN}
+          // url={"https://api.mapbox.com/styles/v1/amply/ckp796vza066p17qw072dnhmr/tiles/256/{z}/{x}/{y}@2x?access_token=" + TOKEN}
+          url={"https://api.mapbox.com/styles/v1/suzieqsharkyamply/ckaz5hl1w1cds1ipe3k4oso6k/tiles/256/{z}/{x}/{y}@2x?access_token=" + TOKEN}
         />
       </MapContainer>
 
       <footer>
-        <div>&#169;2020 AMPLY Power. All Rights Reserved.</div>
+        <div>&#169;2021 AMPLY Power. All Rights Reserved.</div>
         <a href="https://www.amplypower.com/whitepaper2021/">
-          <span>Get the 2020 Report </span>
-          - Unlocking the&nbsp;Cost Saving Potential of Electric Fleets.
+          <span>Get the 2021 Report </span>
+          - Managed Charging Accelerates Costs & Health Benefits of EVs.
         </a>
       </footer>
     </div>
